d1f948831c665f0ad406b94f09e3169c875d1567
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-03-24 21:10:51 +0900

    update

--- 1.go
+++ 1.go
@@ -1,6 +1,24 @@
 package main
-import "fmt"
 
-func main(){
-    fmt.Printf("Hello, world\n")
+import (
+	"fmt"
+	"strings"
+)
+
+type T struct {
+	name  string // オブジェクトの名前
+	value int    // その値
+}
+
+func main() {
+	sum := 0
+	for i := 1; i <= 10; i++ {
+		if i > 5 {
+			s := "iが6以上です"
+			fmt.Printf("%d\n", s)
+			fmt.Printf("%v\n", strings.Fields(s))
+		}
+		sum += i
+	}
+	fmt.Printf("%d\n", sum)
 }
