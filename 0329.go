c783267733dc63ce3e61730c937a949394ee5bb1
daichi <daidaichi1208@gmail.com>
2020-04-03 17:24:52 +0900

    update

--- 0329.go
+++ 0329.go
@@ -7,3 +7,12 @@ func main() {
     // fmtパッケージを使って、「Hello, 世界」と出力するように書き換えてください
     fmt.Println("Hello, 世界")
 }
+package main
+
+func main() {
+	// 「こんにちは、世界」と出力してください
+    println("こんにちは、世界")
+	
+	// この行をコメントにしてください
+
+}
